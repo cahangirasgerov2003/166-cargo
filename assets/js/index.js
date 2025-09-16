e566fe1a3fd3aab7a258761f010fe6fab62c3730
Cahangir Asgerov <109364812+cahangirasgerov2003@users.noreply.github.com>
2025-09-16 22:30:02 +0400

    Change: Style.css

--- assets/js/index.js
+++ assets/js/index.js
@@ -48,5 +48,38 @@ document.querySelectorAll(".headerLeft button").forEach((btn) => {
 document.querySelectorAll(".links li").forEach((listElementLinks) => {
   listElementLinks.addEventListener("click", () => {
     listElementLinks.classList.add("linkClicked");
+
+    document.querySelectorAll(".links li").forEach((check) => {
+      if (check !== listElementLinks) {
+        check.classList.remove("linkClicked");
+      }
+    });
+  });
+});
+
+document.querySelectorAll(".breadcrumbSection ul li a").forEach((selected) => {
+  selected.addEventListener("click", () => {
+    selected.classList.add("chosenOne");
+
+    document.querySelectorAll(".breadcrumbSection ul li a").forEach((check) => {
+      if (check !== selected) {
+        check.classList.remove("chosenOne");
+      }
+    });
+  });
+});
+
+document.querySelectorAll(".necəIsleyirikList div li").forEach((selected) => {
+  selected.addEventListener("click", () => {
+    const firstChild = selected.firstElementChild;
+    selected.classList.add("chosed");
+    firstChild.classList.add("selectedElement");
+
+    document.querySelectorAll(".necəIsleyirikList div li").forEach((check) => {
+      if (check !== selected) {
+        check.classList.remove("chosed");
+        check.firstElementChild.classList.remove("selectedElement");
+      }
+    });
   });
 });
